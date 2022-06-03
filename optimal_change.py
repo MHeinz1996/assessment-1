6dcb49a3e44eba33d96cb4401e673341cd6cd137
MHeinz1996 <michael.heinzinger@gmail.com>
2022-06-03 14:57:29 -0500

    Added a second function to test validity of passed args. Pretty sure I covered all edge/special cases. Assessment Complete.

--- optimal_change.py
+++ optimal_change.py
@@ -1,14 +1,17 @@
 def optimal_change(item_cost, ammount_paid):
     change = {}
    
+    # Calls validity function to test args
+    valid = validity(item_cost, ammount_paid)
+
+    if(valid != True):
+        return valid
+
     # Use round() function and multiply by 100 to mitigate floating point arithmetic errors
     leftover = int(round(ammount_paid, 2)*100) - int(round(item_cost, 2)*100)
-
-    # Handle invalid inputs
-    if(item_cost < 0 or ammount_paid < 0):
-        return f"Invalid parameter. Parameters must be positive numbers"
+    
     if(leftover < 0):
-        return f"Insufficient funds. The item costs ${item_cost} and you only paid ${ammount_paid}"
+        return f"Insufficient funds. The item costs ${item_cost} and you only paid ${ammount_paid}."
     
     # Dict to define monetary denominations with a value
     money = {
@@ -39,11 +42,14 @@ def optimal_change(item_cost, ammount_paid):
     # Initialize an output to be appended to with output formatter
     output = f"The optimal change for an item that costs ${item_cost} with an amount paid of ${ammount_paid} is "
 
-    # Output formatter
+    # Output formatter. Counter variable is used to track where punctuations go.
     counter = 0
     for item in change:
         counter += 1
-        if(counter == len(change)):
+        
+        if(len(change) == 1):
+            pass
+        elif(counter == len(change)):
             output+= "and "
         if(item != 'penny'):
             if change[item] > 1:
@@ -61,11 +67,30 @@ def optimal_change(item_cost, ammount_paid):
             else:
                 output += f"{change[item]} penny."
         
-
-
-
-    
     return output
 
-# Example of output formatting:
-# "The optimal change for an item that costs $62.13 with an amount paid of $100 is 1 $20 bill, 1 $10 bill, 1 $5 bill, 2 $1 bills, 3 quarters, 1 dime, and 2 pennies."
+# Function to test validity of args
+def validity(item_cost, ammount_paid):
+    
+    # Tests to make sure that if arg has decimals, it only goes out to two places
+    if '.' in str(item_cost):
+            decimal_places_cost = str(item_cost).split('.')
+            if (len(decimal_places_cost[1]) > 2):
+                return f"Invalid parameter. Parameters must be either an integer or a floating point number (to two decimal places)."
+
+    if '.' in str(ammount_paid):
+        decimal_places_paid = str(ammount_paid).split('.')
+        if (len(decimal_places_paid[1]) > 2):
+            return f"Invalid parameter. Parameters must be either an integer or a floating point number (to two decimal places)."
+
+    # Makes sure that args passed are positive integers or floats
+    if((isinstance(item_cost, int) == True or isinstance(item_cost, float) == True) and (isinstance(ammount_paid, int) == True or isinstance(ammount_paid, float))):
+        pass
+    else:
+        return f"Invalid parameter. Parameters must be either an integer or a floating point number (to two decimal places)."
+
+    if(item_cost < 0 or ammount_paid < 0):
+        return f"Invalid parameter. Parameters must be positive numbers."
+
+    # If all tests pass, args are valid
+    return True
--- optimal_change_spec.py
+++ optimal_change_spec.py
@@ -1,3 +1,12 @@
 from optimal_change import optimal_change
 
-print(optimal_change(62.13, 100))
+
+print("1:", optimal_change(62.13, 100) == "The optimal change for an item that costs $62.13 with an amount paid of $100 is 1 $20 bill, 1 $10 bill, 1 $5 bill, 2 $1 bills, 3 quarters, 1 dime, and 2 pennies.")
+print("2:", optimal_change(31.51, 50) == "The optimal change for an item that costs $31.51 with an amount paid of $50 is 1 $10 bill, 1 $5 bill, 3 $1 bills, 1 quarter, 2 dimes, and 4 pennies.")
+print("3:", optimal_change(36.23, 20.67) == "Insufficient funds. The item costs $36.23 and you only paid $20.67.")
+print("4:", optimal_change(36.23, -20.67) == "Invalid parameter. Parameters must be positive numbers.")
+print("5:", optimal_change(36.23, '50.67') == "Invalid parameter. Parameters must be either an integer or a floating point number (to two decimal places).")
+print("6:", optimal_change(93.121234, 123.42353) == "Invalid parameter. Parameters must be either an integer or a floating point number (to two decimal places).")
+print("7:", optimal_change(.01, .02) == "The optimal change for an item that costs $0.01 with an amount paid of $0.02 is 1 penny.")
+print("This next test might take a little bit longer:")
+print("8:", optimal_change(123456789, 987654321) == "The optimal change for an item that costs $123456789 with an amount paid of $987654321 is 8641975 $100 bills, 1 $20 bill, 1 $10 bill, and 2 $1 bills.")
